b61f1ae7ebc1da102712802b8e292ef5e94570f5
AyushBotz <129131041+AyushBotz@users.noreply.github.com>
2023-03-29 00:13:13 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,12 +3,12 @@ from os import path, getenv
 SUDO = "5171347305"
 
 class Config:
-    API_ID = int(getenv("API_ID", "14055090"))
-    API_HASH = getenv("API_HASH", "a46f7b439d0afa45b7a69fc450f754e9")
-    BOT_TOKEN = getenv("BOT_TOKEN", "5616982320:AAFk0OhEkbFuUMrSSs6dfLdBYhxUI0c9Pt4")
-    FSUB = getenv("FSUB", "EmoBotDevolopers")
-    CHID = int(getenv("CHID", "-1001551504918"))
-    SUDO = list(map(int, getenv("SUDO").split()))
+    API_ID = int(getenv("API_ID", "12897895"))
+    API_HASH = getenv("API_HASH", "66009ef52a2da9cea6a7015e59b556ca")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6192626369:AAF1NpZg8035O3qou-8g9P6ssk1tuaLN8o8")
+    FSUB = getenv("FSUB", "PiroHackz")
+    CHID = int(getenv("CHID", "-1001545900924"))
+    SUDO = list(map(int, getenv("SUDO").split("5822067254")))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     
 cfg = Config()
